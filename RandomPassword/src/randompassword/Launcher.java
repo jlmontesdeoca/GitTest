67cf3c78a2caba2f5055cfb3ecc03d028e9573fb
Jose_Luis Montes_de_Oca <jlmontesdeoca@yahoo.com>
2012-11-20 19:41:44 -0430

    * Set output to one per line.

--- RandomPassword/src/randompassword/Launcher.java
+++ RandomPassword/src/randompassword/Launcher.java
@@ -8,7 +8,7 @@ public class Launcher {
    public static void main(String[] args) {
       RandomPassword rp = new RandomPassword();
       for (int i = 0; i < 100; i++) {
-         System.out.print(rp.createPassword(14) + "  ");
+         System.out.println(rp.createPassword(14));
       }
    }
 }
